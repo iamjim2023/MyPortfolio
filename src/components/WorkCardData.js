843b4236bf3c076bbd5eefa19d698a117807f7eb
Jimuel Madlangsakay <jmuelkoh22@gmail.com>
2023-02-23 11:36:21 +0800

    Update

---
+++ src/components/WorkCardData.js
@@ -0,0 +1,26 @@
+import pro1 from "../assets/project1.png";
+import pro2 from "../assets/project2.jpg";
+import pro3 from "../assets/project3.jpg";
+
+const ProjectCardData = [
+  {
+    imgsrc: pro1,
+    title: "Web Development Project Idea",
+    text: "Organize pages, attachments, and even spaces with keyword labels — such as meeting type or project name. Use the advanced search & page tree to quickly find and label key docs in a well-organized page tree. Use the Table of Contents macro to help colleagues quickly locate information.",
+    view: "url.com",
+  },
+  {
+    imgsrc: pro2,
+    title: "Some Points To Consider About Web Development",
+    text: "Are you a beginner in the world of web applications and development? There is nothing to be afraid of. Prepare yourself to handle the frontend backend and tools perfectly, rest all you will learn with different projects. It is a fact that every new project teaches you the skill to resolve problems.",
+    view: "url.com",
+  },
+  {
+    imgsrc: pro3,
+    title: "Web Development Project Idea V2",
+    text: "With the dramatic growth of web development and programming in the job market and industry, the demand for web developers and programmers has increased dramatically. One of the best ways to improve your web programming skills is to do different projects in a practical way. ",
+    view: "url.com",
+  },
+];
+
+export default ProjectCardData;
