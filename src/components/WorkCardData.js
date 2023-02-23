90bfb72e1e857aeab4090070a77afc2430c63c09
Jimuel Madlangsakay <jmuelkoh22@gmail.com>
2023-02-23 12:52:22 +0800

    Update

--- src/components/WorkCardData.js
+++ src/components/WorkCardData.js
@@ -5,21 +5,21 @@ import pro3 from "../assets/project3.jpg";
 const ProjectCardData = [
   {
     imgsrc: pro1,
-    title: "Web Development Project Idea",
-    text: "Organize pages, attachments, and even spaces with keyword labels — such as meeting type or project name. Use the advanced search & page tree to quickly find and label key docs in a well-organized page tree. Use the Table of Contents macro to help colleagues quickly locate information.",
-    view: "url.com",
+    title: "Web Development Project Idea.",
+    text: "Organize pages, attachments, and even spaces with keyword labels, such as meeting type or project name. Use the advanced search & page tree to quickly find and label key docs in a well-organized page tree. Use the Table of Contents macro to help colleagues quickly locate information.",
+    view: "https://www.atlassian.com/software/confluence?&aceid=&adposition=&adgroup=141338646186&campaign=18446321204&creative=647939325599&device=c&keyword=webdevelopment&matchtype=p&network=g&placement=&ds_kids=p73335773505&ds_e=GOOGLE&ds_eid=700000001542923&ds_e1=GOOGLE&gclid=CjwKCAiAl9efBhAkEiwA4TorimFpFPpwQoNYSW-H5sJ6tuu92ZAIwlQ4YWpTsLvC031tAUCggSs5TRoC6tYQAvD_BwE&gclsrc=aw.ds",
   },
   {
     imgsrc: pro2,
-    title: "Some Points To Consider About Web Development",
+    title: "Some Points To Consider About Web Development.",
     text: "Are you a beginner in the world of web applications and development? There is nothing to be afraid of. Prepare yourself to handle the frontend backend and tools perfectly, rest all you will learn with different projects. It is a fact that every new project teaches you the skill to resolve problems.",
-    view: "url.com",
+    view: "https://www.google.com/",
   },
   {
     imgsrc: pro3,
-    title: "Web Development Project Idea V2",
+    title: "Web Development Project Idea V2.",
     text: "With the dramatic growth of web development and programming in the job market and industry, the demand for web developers and programmers has increased dramatically. One of the best ways to improve your web programming skills is to do different projects in a practical way. ",
-    view: "url.com",
+    view: "https://blog.faradars.org/%d9%85%d9%88%d8%b6%d9%88%d8%b9-%d8%a8%d8%b1%d8%a7%db%8c-%d8%b7%d8%b1%d8%a7%d8%ad%db%8c-%d9%88%d8%a8-%d8%b3%d8%a7%db%8c%d8%aa/",
   },
 ];
 
